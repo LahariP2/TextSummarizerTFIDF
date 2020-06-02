8caf6c121204d3a778e6436588c21aba7c3b5f5e
lahari <donut8pi@gmail.com>
2020-06-02 16:57:32 -0700

    Wrote functions to split passage into sentences and words.

--- tfidf.py
+++ tfidf.py
@@ -1,15 +1,40 @@
 import re
 text_file = open("test_text.txt", "r")
 
-s = text_file.read()
-
+s = text_file.read() 
+p = s.replace('\n', '')
 
 def split_by_sentence():
-    print(s)
-    p = s.replace('\n', '')
-    print(p)
-    re.split(r'. |, |! |? ', p)
-    
 
-split_by_sentence()
+    all_sent = re.split(r'\. |\, |\! |\? |\.', p)
+    all_sent.pop()
 
+    return all_sent
+
+def split_into_words():
+    all_wrds = re.split(r'\. |\, |\! |\? |\.|\s+', p)
+    all_wrds.pop()
+
+    return all_wrds
+
+def get_stop_words_into_list():
+    with open('stop_words.txt') as f:
+        stop_wrds = f.read().splitlines()
+    return stop_wrds
+
+def get_non_stop(all_wrds, stop_wrds): 
+    non_stop_wrds = list()
+    for s in all_wrds: 
+        if s not in stop_wrds:
+            non_stop_wrds.append(s)
+    return non_stop_wrds
+        
+
+all_sentences = split_by_sentence()
+print(all_sentences)
+all_words = split_into_words()
+print(all_words)
+
+#stop_words = get_stop_words_into_list()
+#non_stop_words = get_non_stop(all_words, stop_words)
+#print(non_stop_words)
