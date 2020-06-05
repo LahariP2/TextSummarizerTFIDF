bdaaeda9dbc60e8434492f06b45b38f2db7e5c9b
riyv <riyag3@illinois.edu>
2020-06-05 18:36:26 -0500

    fixed styling

--- tfidf.py
+++ tfidf.py
@@ -1,40 +1,55 @@
 import re
-text_file = open("test_text.txt", "r")
+#tf = (frequency of the term in the doc/total number of terms in the doc)
+#normalization of tf(average of duplicate tf scores/absolute word count in all docs)
+#idf = ln(total number of docs/number of docs with term in it)
+#info on tfidf from http://www.tfidf.com/ but all functions written by me
 
-s = text_file.read() 
-p = s.replace('\n', '')
+def readFile(path):
+    with open(path, "rt") as f:
+        text = f.read()
+    return text
 
-def split_by_sentence():
+class PracticeNLP(object):
+    def __init__(self, path):
+        self.path = path
+        self.text = readFile(self.path)
+        self.punctuation = set(['.', '?', '!'])
+        self.textList = []
+        self.symbols = set([':', ';', ',', '-'])
+    
+    def removeStopwords(self):
+        stopwordString = readFile('stop_words.txt')
+        self.stopwordsList = stopwordString.split()
 
-    all_sent = re.split(r'\. |\, |\! |\? |\.', p)
-    all_sent.pop()
+    def cleanWord(self, word):
+        word = word.lower()
+        charList = list(word)
+        newCharList = []
+        for char in charList:
+            if char not in self.symbols:
+                newCharList.append(char)
+        word = ''.join(newCharList)
+        return word
 
-    return all_sent
+    def splitIntoSentences(self):
+        dirtyList = re.split('\. |\! |\? |\.|\n', self.text)
+        for elem in dirtyList:
+            if elem != '':
+                elem = self.cleanWord(elem)
+                self.textList.append(elem)
+    
+    def tfCalc(self, sentence):
+        sentenceTfScores = dict()
+        wordList = sentence.split()
+        for word in wordList:
+            sentenceTfScores[word] = wordList.count(word)/len(wordList)
+        return sentenceTfScores
 
-def split_into_words():
-    all_wrds = re.split(r'\. |\, |\! |\? |\.|\s+', p)
-    all_wrds.pop()
+NLPDemo = PracticeNLP('test_text.txt')
+NLPDemo.removeStopwords()
+NLPDemo.splitIntoSentences()
+for sentence in NLPDemo.textList:
+    print(NLPDemo.tfCalc(sentence))
 
-    return all_wrds
 
-def get_stop_words_into_list():
-    with open('stop_words.txt') as f:
-        stop_wrds = f.read().splitlines()
-    return stop_wrds
 
-def get_non_stop(all_wrds, stop_wrds): 
-    non_stop_wrds = list()
-    for s in all_wrds: 
-        if s not in stop_wrds:
-            non_stop_wrds.append(s)
-    return non_stop_wrds
-        
-
-all_sentences = split_by_sentence()
-print(all_sentences)
-all_words = split_into_words()
-print(all_words)
-
-#stop_words = get_stop_words_into_list()
-#non_stop_words = get_non_stop(all_words, stop_words)
-#print(non_stop_words)
