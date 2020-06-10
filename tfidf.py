35b3746e4530717bbe26731b7b4b7299c10ab9a8
riyv <riyag3@illinois.edu>
2020-06-10 16:51:46 -0500

    fixed td/idf, added tfidf calculation

--- tfidf.py
+++ tfidf.py
@@ -11,12 +11,12 @@ def readFile(path):
 class PracticeNLP(object):
     # Constructor to initialize variables
     def __init__(self, path):
-        print("HIIIIIIIIIIIIIII")
-
         self.path = path
         self.text = readFile(self.path)
         self.punctuation = set(['.', '?', '!'])
         self.symbols = set([':', ';', ',', '-'])
+        self.fullText = readFile(path)
+    
         self.stopWords = self.stopWordsIntoList()
         self.sentList = self.splitIntoSentences(self.text, self.symbols)
         self.wordsList = self.splitIntoWords(self.sentList, self.symbols)
@@ -24,7 +24,6 @@ class PracticeNLP(object):
         
     # Converts the data in stop_words.txt to a string and returns them as a list of individual stop words
     def stopWordsIntoList(self):
-        print("STOP WORDS INTO LIST")
         stopwordString = readFile('stop_words.txt')
         return stopwordString.split()
 
@@ -41,9 +40,7 @@ class PracticeNLP(object):
 
     # Returns and array containing the text split into sentences
     def splitIntoSentences(self, fullText, symbols):
-        print("SENTENCE SPLIT")
-
-        sentences = re.split(r'\. |\! |\? |\.|\n', fullText)
+        sentences = re.split(r'\. |\! |\!|\? |\?|\.|\n', fullText)
         for elem in sentences:
             if elem != '':
                 elem = self.cleanWord(elem, symbols)
@@ -51,19 +48,16 @@ class PracticeNLP(object):
 
     # Returns an arry containing every word in the full text
     def splitIntoWords(self, sentences, symbols):
-        print("Words SPLIT")
-
         words = list()
         for sent in sentences:
-            words = sent.split()
-            for w in words: 
-                self.cleanWord(w, symbols)
+            words_in_sent = sent.split()
+            for w in words_in_sent: 
+                w = self.cleanWord(w, symbols)
                 words.append(w)
         return words
 
     # Returns a list of all the words in the text that are not stop words
     def getNonStopWords(self, words, stopWords): 
-        print("NON ")
         nonStopWords = list()
         for word in words: 
             if word not in stopWords:
@@ -71,30 +65,47 @@ class PracticeNLP(object):
         return nonStopWords
 
     # Calculates and returns a dictionary for the TF of each word in the text
-    # TF of each word = (number of times word appears in its sentence) / (number of words in the sentence)
+    # TF of each word = (number of times word appears in its sentence) / (total number of words in the sentence)
     def tfCalc(self, sentences):
         sentenceTfScores = dict()
         for sent in sentences:   
             wordList = sent.split()
+            wordList = [element.lower() for element in wordList]
             for word in wordList:
-                sentenceTfScores[word] = wordList.count(word)/len(wordList)
+                sentenceTfScores[word] = (sentenceTfScores.get(word, 0)) + (wordList.count(word)/len(wordList))
+        
+        for key in sentenceTfScores:
+            sentenceTfScores[key] = (sentenceTfScores.get(key, 0)) / (self.wordsList.count(key))
+        print(sentenceTfScores)
         return sentenceTfScores
 
     # Calculates and returns a dictionary for the IDF 
-    # IDF of each word = (number of sentences) / (number of sentences that contain the word)
+    # IDF of each word = (number of sentences) / (number of sentences that contain the word in document)
     def idfCalc(self, words, sentences): 
         documentIdfScores = dict()
         wordCount = 0
         for word in words:
             for sent in sentences: 
                 wordList = sent.split()
+                wordList = [element.lower() for element in wordList]
                 wordCount += wordList.count(word)
-            documentIdfScores[word] = math.log10(sentences.len()/wordCount)
+            documentIdfScores[word] = math.log10((len(sentences))/wordCount)
             wordCount = 0
+
+        for key in documentIdfScores:
+            documentIdfScores[key] = (documentIdfScores.get(key, 0)) / (self.wordsList.count(key))
+        print(documentIdfScores)
         return documentIdfScores
 
-NLPDemo = PracticeNLP('test_text.txt')
+    def tfidfCalc(self, tf, idf):
+        tfidf = dict()
+        for word in self.wordsList:
+            tfidf[word] = tf[word] * idf[word]
+        print(tfidf)
+        return tfidf
+            
 
-NLPDemo.tfCalc(NLPDemo.sentList)
-NLPDemo.idfCalc(NLPDemo.wordsList, NLPDemo.sentList)
-# Change to commit
+NLPDemo = PracticeNLP('test_text.txt')
+tf_dict = NLPDemo.tfCalc(NLPDemo.sentList)
+idf_dict = NLPDemo.idfCalc(NLPDemo.wordsList, NLPDemo.sentList)
+NLPDemo.tfidfCalc(tf_dict, idf_dict)
